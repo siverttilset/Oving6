8826f0f16e043c28f050f81b3d13476e3c09a0e9
TordEA02 <toe.andreassen@stud.uis.no>
2024-10-07 13:27:01 +0200

    test
    ikke fetch

--- Oving6.py
+++ Oving6.py
@@ -1,7 +1,8 @@
 
-
 import csv
 from datetime import datetime
+
+from numpy import column_stack
 
 # Define lists for storing data
 sola_dates = []
@@ -9,13 +10,19 @@ sola_temperatures = []
 sola_pressures = []
 
 # Read the Sola station data
-with open('temperatur_trykk_met_samme_rune_time_datasett.txt', mode='r', encoding='utf-8') as file:
+with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r', encoding='utf-8') as file:
     reader = csv.reader(file, delimiter=';')
     next(reader)  # Skip header
     for row in reader:
+<<<<<<< Updated upstream
         date_str = row[2]  # Date and time
         temperature = float(row[4].replace(',', '.'))  # Temperature
         pressure = float(row[5].replace(',', '.'))  # Pressure
+=======
+        date_str = column_stack[2]  # Date and time
+        temperature = float(column_stack[4].replace(',', '.'))  # Temperature
+        pressure = float(column_stack[5].replace(',', '.'))  # Pressure
+>>>>>>> Stashed changes
         
         # Parse the date and time
         date_time = datetime.strptime(date_str, '%d.%m.%Y %H:%M')
@@ -47,10 +54,13 @@ print(sola_dates[:5], sola_temperatures[:5], sola_pressures[:5])
 
 
 
+<<<<<<< Updated upstream
 
 
 
 
 
+=======
+>>>>>>> Stashed changes
 tester23213213213
 sdf
